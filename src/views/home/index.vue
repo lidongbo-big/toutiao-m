a19250cb6752e866bc0339e0205b3aa4bc0f7cbe
lidongbo-big <1144086550@qq.com>
2020-05-17 10:46:21 +0800

    数据持久化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -5,26 +5,45 @@
     </van-nav-bar>
     <van-tabs class="channel-tabs" v-model="active">
       <van-tab :title="channel.name" v-for="channel in channels" :key="channel.id"> <article-list :channel="channel" /> </van-tab>
+      <div slot="nav-right" class="wap-nav-placeholder"></div>
+      <div @click="isChannelEditShow = true" class="wap-nav-wrap" slot="nav-right"><van-icon name="bars" /></div>
     </van-tabs>
+    <van-popup
+    v-model="isChannelEditShow"
+    closeable
+    position="bottom"
+    class="channel-deit-popup"
+    close-icon-position="top-left"
+    get-container="body"
+    style="height: 100%">
+    <channel-edit :user-channels='channels' :active="active" @close="isChannelEditShow = false" @update-active="active = $event" />
+  </van-popup>
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/user'
 import ArticleList from './components/article-list'
+import ChannelEdit from './components/channel-edit'
+import { mapState } from 'vuex'
+import { getItem } from '@/utils/storage'
 export default {
   name: 'HomeIndex',
   components: {
-    ArticleList
+    ArticleList,
+    ChannelEdit
   },
   props: {},
   data () {
     return {
       active: 0,
-      channels: []
+      channels: [],
+      isChannelEditShow: false // 控制编辑频道的显示状态
     }
   },
-  computed: {},
+  computed: {
+    ...mapState(['user'])
+  },
   watch: {},
   created () {
     this.loadChannels()
@@ -32,9 +51,23 @@ export default {
   mounted () {},
   methods: {
     async loadChannels () {
-      const { data } = await getUserChannels()
-      this.channels = data.data.channels
-      console.log(data)
+      let channels = []
+      if (this.user) {
+        const { data } = await getUserChannels()
+        channels = data.data.channels
+      } else {
+        const localChannels = getItem('user-channels')
+        if (localChannels) {
+          channels = localChannels
+        } else {
+          const { data } = await getUserChannels()
+          channels = data.data.channels
+        }
+      }
+      this.channels = channels
+      // const { data } = await getUserChannels()
+      // this.channels = data.data.channels
+      // console.log(data)
     }
   }
 }
@@ -67,6 +100,34 @@ export default {
       width: 15px !important;
       height: 3px;
       background: #3296fa;
+    }
+  }
+  .wap-nav-placeholder {
+    width: 33px;
+    flex-shrink: 0;
+  }
+  .wap-nav-wrap {
+    position: fixed;
+    right: 0;
+    width: 33px;
+    height: 43px;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    opacity: .9;
+    .van-icon {
+      font-size: 24px;
+    }
+    &:before {
+      content: '';
+      width: 1px;
+      background: url("./line.png") no-repeat;
+      background-size: contain;
+      position: absolute;
+      left: 0;
+      top: 0;
+      bottom: 0;
     }
   }
 }
